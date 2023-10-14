7d7b15d837f526aa325bf5d9693402ba849d367f
Myoungdo Park <cuspymd@gmail.com>
2023-10-14 20:27:16 +0900

    Add jack_tokenizer test

--- jack_compiler/test_jack_tokenizer.py
+++ jack_compiler/test_jack_tokenizer.py
@@ -1,6 +1,25 @@
 import unittest
 
+from jack_compiler.jack_tokenizer import JackTokenizer
+
 
 class TestJackTokenizer(unittest.TestCase):
-    def test(self):
-        pass
+    def test_has_more_tokens_given_empty_line(self):
+        tokenizer = JackTokenizer("")
+        self.assertFalse(tokenizer.has_more_tokens())
+
+        tokenizer = JackTokenizer("	")
+        self.assertFalse(tokenizer.has_more_tokens())
+
+        tokenizer = JackTokenizer("\n   \n     \n")
+        self.assertFalse(tokenizer.has_more_tokens())
+
+        tokenizer = JackTokenizer("\r\n   \r\n     \r\n")
+        self.assertFalse(tokenizer.has_more_tokens())
+
+        tokenizer = JackTokenizer("\n   \n     return;\n")
+        self.assertTrue(tokenizer.has_more_tokens())
+
+    def test_has_more_tokens_given_comment(self):
+        tokenizer = JackTokenizer("// comment")
+        self.assertFalse(tokenizer.has_more_tokens())
