812249516c02c827453482a42bf4fdb7b8e8003e
Myoungdo Park <cuspymd@gmail.com>
2023-10-15 00:34:25 +0900

    Implementing has_more_tokens

---
+++ jack_compiler/jack_tokenizer.py
@@ -0,0 +1,65 @@
+import re
+from typing import List
+
+
+class JackTokenizer:
+    SYMBOLS = {
+        "{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~"
+    }
+
+    def __init__(self, file_text: str):
+        self.tokens = self._parse_tokens(file_text)
+        self.current_token_number = -1
+
+    def _parse_tokens(self, file_text: str) -> List[str]:
+        lines = self._get_valid_lines(file_text)
+        tokens = []
+
+        for line in lines:
+            token_type = -1
+            token_start_index = 0
+
+            for i in range(len(line)):
+                if token_type == -1:
+                    token_start_index = i
+
+                if line[i] in JackTokenizer.SYMBOLS:
+                    if token_type == 0:
+                        tokens.append(line[token_start_index:i])
+                        token_type = -1
+
+                    tokens.append(line[i])
+                elif line[i] == " ":
+                    if token_type == 0:
+                        tokens.append(line[token_start_index:i])
+                        token_type = -1
+                else:
+                    token_type = 0
+
+            if token_type == 0:
+                tokens.append(line[token_start_index:len(line)])
+
+        return tokens
+
+    def _get_valid_lines(self, file_text: str) -> List[str]:
+        file_text = self._delete_comments(file_text)
+        return [
+            valid_text
+            for line in file_text.splitlines()
+            if (valid_text := self._get_valid_text(line))
+        ]
+
+    def _delete_comments(self, text: str) -> str:
+        comment_regex = r"/\*.*?\*/"
+        text = re.sub(comment_regex, "", text, flags=re.DOTALL)
+        comment_regex = r"//.*"
+        return re.sub(comment_regex, "", text)
+
+    def _get_valid_text(self, text: str) -> str:
+        return text.strip()
+
+    def has_more_tokens(self):
+        return self.current_token_number < len(self.tokens)-1
+
+    def advance(self):
+        self.current_token_number += 1
--- jack_compiler/test_jack_tokenizer.py
+++ jack_compiler/test_jack_tokenizer.py
@@ -20,6 +20,35 @@ class TestJackTokenizer(unittest.TestCase):
         tokenizer = JackTokenizer("\n   \n     return;\n")
         self.assertTrue(tokenizer.has_more_tokens())
 
-    def test_has_more_tokens_given_comment(self):
+    def test_has_more_tokens_given_one_line_comment(self):
         tokenizer = JackTokenizer("// comment")
+        self.assertFalse(tokenizer.has_more_tokens())
+
+    def test_has_more_tokens_given_multi_line_comment(self):
+        tokenizer = JackTokenizer("/* comment\nreturn;\n*/")
+        self.assertFalse(tokenizer.has_more_tokens())
+
+    def test_has_more_tokens_given_symbols(self):
+        symbol_text = "{}()[].,;+-*/&|<>=~"
+        tokenizer = JackTokenizer(symbol_text)
+
+        self._verify_has_more_tokens(tokenizer, symbol_text)
+
+    def test_has_more_tokens_given_symbolsWithSpace(self):
+        symbol_text = "{}()[] .,;+    -*/&   |<>=~"
+        tokenizer = JackTokenizer(symbol_text)
+
+        self._verify_has_more_tokens(tokenizer, range(19))
+
+    def test_has_more_tokens_given_symbolsWithNewLine(self):
+        symbol_text = "{}()[].,;+\n-*/&\n|<>=~"
+        tokenizer = JackTokenizer(symbol_text)
+
+        self._verify_has_more_tokens(tokenizer, range(19))
+
+    def _verify_has_more_tokens(self, tokenizer, iterator):
+        for _ in iterator:
+            self.assertTrue(tokenizer.has_more_tokens())
+            tokenizer.advance()
+
         self.assertFalse(tokenizer.has_more_tokens())
